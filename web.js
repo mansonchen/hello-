c498f144b998660a2e45dbe31c0b42a1079625eb
mansonchen <38245020+mansonchen@users.noreply.github.com>
2018-04-13 14:53:36 +0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.get('/', function(req, res) {
+    res.send('HerokuでNode.jsとExpressを使ってHello world!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("Listening on " + port);
+});
+
